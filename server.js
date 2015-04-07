2b41fbbcc072642042b608446253d4a37124d578
guspowell <augustuspowell@me.com>
2015-04-07 12:33:09 +0100

    setup the server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+server.listen(3000, function(){
+  console.log("Server listening on port 3000")
+});
+
+app.get('/', function(request, response){
+  response.send("hello world");
+});
+
+module.exports = server;
