2765d9660af7ed60a0df7c91d1e26fa6e9f22f18
guspowell <augustuspowell@me.com>
2015-04-08 14:44:51 +0100

    added a hello world

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.set('view engine', 'ejs');
+app.use('/stylesheets', express.static(__dirname + '/public/stylesheets'));
+app.use('/javascripts', express.static(__dirname + '/public/javascripts'));
+
 server.listen(3000, function(){
   console.log("Server listening on port 3000")
 });
